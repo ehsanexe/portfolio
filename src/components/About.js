9b3540a4842ee225437dd68db929c199e99e566e
Ehsan <ehsanexe01@gmail.com>
2023-07-07 21:09:26 +0500

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import { Grid, Paper } from "@mui/material";
+import React from "react";
+import me from "../assets/me.png";
+import { Container } from "@mui/system";
+import "./about.css";
+
+export default function About() {
+  return (
+    <div className="aboutContainer">
+      <div className="profileImage">
+        <img src={me} />
+        <div className="glass" />
+        <div className="lines" />
+      </div>
+      <div>
+        <p>
+          Hi, my name is <h1>Muhammad Ehsan Shafique</h1> I'm a Sofware Engineer
+          graduated from NED University in Software Engineering. I enjoy
+          building digital solutions. Currently my main expertise is in app
+          building using technologies like react native. I also enjoy working in
+          MERN stack in my spare time.
+        </p>
+      </div>
+    </div>
+  );
+}
